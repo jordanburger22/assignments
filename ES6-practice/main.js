b7a0aa280ff7e25e221a96be17e151d66ff3ab0a
Jordan Burger <jordanburger22@gmail.com>
2022-11-03 08:15:04 -0600

    finished assignment

---
+++ ES6-practice/main.js
@@ -0,0 +1,32 @@
+// const name = "John"
+// const age = 101
+
+// function runForLoop(pets) {
+//     let petObjects = []
+//     for (let i = 0; i < pets.length; i++) {
+//         let pet = { type: pets[i] }
+//         let name;
+//         if (pets[i] === "cat") {
+//             name = "fluffy"
+//         } else {
+//             name = "spot"
+//         }
+//         console.log("pet name: ", name)
+//         pet.name = name
+//         petObjects.push(pet)
+//     }
+//     console.log("man name: ", name)
+//     return petObjects
+// }
+
+// runForLoop(["cat", "dog"])
+
+
+const carrots = ["bright orange", "ripe", "rotten"]
+
+function mapVegetables(arr) {
+    return arr.map((carrot) => {
+        return { type: "carrot", name: carrot }
+    })
+}
+
